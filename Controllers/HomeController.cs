7af0c9fa1b02f3db88d34db23cb8cdf6e2880079
kshitij.lingthep <kshitij.lingthep@fii-usa.com>
2020-10-09 17:12:06 -0500

    adding files
    ttt
    
    final
    
    git test
    
    test1 fdevelpo
    
    update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -38,5 +38,10 @@ namespace MyProject.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+                public IActionResult Ifndex()
+        {
+            return View();
+        }
     }
 }
